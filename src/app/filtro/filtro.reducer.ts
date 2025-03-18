972adc242e5f62d7749155f3453b70f314d70766
Guido Perman <guidoyaelperman@gmail.com>
2025-03-18 10:49:32 -0300

    refactor: clean up filtro reducer formatting and ensure consistent code style

--- src/app/filtro/filtro.reducer.ts
+++ src/app/filtro/filtro.reducer.ts
@@ -4,10 +4,10 @@ import { filtrosValidos, setFilter } from './filtro.action';
 export const initialState: filtrosValidos = 'all'
 
 const _filtroReducer = createReducer<filtrosValidos, Action>(
-    initialState,
-    on(setFilter, (state, { filter }) => filter )    ,
-  );
-  
-  export function filtroReducer(state: any, action: any) {
-    return _filtroReducer(state, action);
-  }
+  initialState,
+  on(setFilter, (state, { filter }) => filter),
+);
+
+export function filtroReducer(state: any, action: any) {
+  return _filtroReducer(state, action);
+}
