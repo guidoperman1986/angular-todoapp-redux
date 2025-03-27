ab834c27f5a3ea4939e383c65675c6f440e7cce2
Guido Perman <guidoyaelperman@gmail.com>
2025-03-27 10:21:39 -0300

    test: add unit tests for toggleAll, edit, and deleteAll actions in todos reducer

--- src/app/todos/todos.reducer.spec.ts
+++ src/app/todos/todos.reducer.spec.ts
@@ -1,5 +1,5 @@
 import { Todo } from "./models/todo.model";
-import { crear, toggle } from "./todos.action";
+import { crear, deleteAll, edit, toggle, toggleAll } from "./todos.action";
 import { todoReducer, initialState } from "./todos.reducer";
 
 describe('todos reducer', () => {
@@ -15,13 +15,52 @@ describe('todos reducer', () => {
 
     it('should act when toggling todo', () => {
         const idToToggle = initialState[0].id;
-        console.log(idToToggle)
         const action = toggle({ id: idToToggle });
         const state = todoReducer(initialState, action);
-        
+
         const toggledTodo = state.find(todo => todo.id === idToToggle);
         expect(toggledTodo).toBeTruthy();
         expect(toggledTodo?.completado).toBe(!initialState[0].completado);
+    })
+
+    it('should toggle all todos', () => {
+        const action = toggleAll({ completed: true });
+        const state = todoReducer(initialState, action);
+
+        state.forEach(todo => {
+            expect(todo.completado).toBeTruthy();
+        })
+
+    })
+
+    it('should edit todo', () => {
+        const newText = 'this is edited text'
+        const idToEdit = initialState[0].id;
+        const action = edit({ id: idToEdit, texto: newText });
+        const state = todoReducer(initialState, action);
+
+        const editedTodo = state.find(todo => todo.id === idToEdit);
+        expect(editedTodo).toBeTruthy();
+        expect(editedTodo?.texto).toBe(newText);
+    })
+
+    it('delete All completed todos', () => {
+        const customInitialState: Todo[] = [
+            { id: 1, texto: 'Todo 1', completado: false },
+            { id: 2, texto: 'Todo 2', completado: true },
+            { id: 3, texto: 'Todo 3', completado: false },
+            { id: 4, texto: 'Todo 4', completado: true },
+        ];
+
+        const action = deleteAll();
+        const state = todoReducer(customInitialState, action);
         
+        expect(state.length).toBe(2);
+        expect(state.every(todo => !todo.completado)).toBe(true);
+        expect(state).toEqual([
+            { id: 1, texto: 'Todo 1', completado: false },
+            { id: 3, texto: 'Todo 3', completado: false },
+        ]);
+
     })
 })
